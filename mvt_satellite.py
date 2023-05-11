1219bcdb80b4ed39dacd6bd29ebfaea35e605a81
timothy <timothy@timothy-X705UA>
2023-05-11 22:18:45 +0200

    Add schema semi implicite and graphes

--- mvt_satellite.py
+++ mvt_satellite.py
@@ -7,7 +7,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def euler_explicite(T, N, y_init, f):
+def schema_euler_explicite(T, N, y_init, f):
     # Initialisation
     y = np.zeros((len(y_init), N+1))
     y[:,0] = y_init
@@ -22,27 +22,67 @@ def euler_explicite(T, N, y_init, f):
         
     return t, y
 
+def schema_semi_implicite(T, N, y_init, f):
+    # Initialisation
+    y = np.zeros((len(y_init), N+1))
+    y[:,0] = y_init
+    h = T / N
+    t = np.linspace(0, T, num=N+1)
+    
+    # Itération
+    for n in range(N):
+        y[:, n+1] = y[:,n] / (1 -  h * f(t[n], y[:,n]))
+        
+    return t, y
+
+def erreur(exacte, approx):
+    return 
+
 if __name__ == "__main__":
     # Constantes
     m = 1 # masse du soleil
     G = 1 # Constante gravitationnelle
-    T = 1
-    N = 1
-    y_init = np.array([3, -4, -2, -1])
+    T = 25
+    N = 2500
+    y_init = np.array([1., 1., 0., 1.])
     
     # On considère l'équation Y'=f(t, Y1, Y2) où Y1, Y2 \in \R^2 et Y = (y, y') et on cherche à trouver y \in \R^2
     f = lambda t, q: np.array([q[2], q[3], 
-        -m * G * q[0] / ((q[0]**2 + q[1]**2)**(3/2)), 
-        -m * G * q[1] / ((q[0]**2 + q[1]**2)**(3/2))])
-    subdivision, approx = euler_explicite(T, N, y_init, f)
+        -m * G * (q[0] / ((q[0]**2 + q[1]**2)**(3/2))), 
+        -m * G * (q[1] / ((q[0]**2 + q[1]**2)**(3/2)))])
+    subdivision, approx_euler = schema_euler_explicite(T, N, y_init, f)
+    _, approx_semi_implicite = schema_semi_implicite(T, N, y_init, f)
+    
+    # Représentations graphiques
+    fig, ax = plt.subplots(3)
     
     # Graphique de la trajctoires de la comète
-    plt.plot(approx[0], approx[1])
-    plt.title("Trajectoires de la comète")
-    plt.tight_layout()
-    plt.show()
+    ax[0].plot(approx_euler[0], approx_euler[1], label="euler explicite")
+    # ax[0].plot(approx_semi_implicite[0], approx_semi_implicite[1], label="semi_implicite")
+    ax[0].set_title("Trajectoires de la comète")
+    ax[0].set_xlabel("position sur l'axe q1")
+    ax[0].set_ylabel("position sur l'axe q2")
+    ax[0].legend()
+    
+    # Graphique de la vitesse de la comète en fonction du temps
+    ax[1].plot(subdivision, approx_euler[2], label="vitesse q1 euler")
+    ax[1].plot(subdivision, approx_euler[3], label="vitesse q2 euler")
+    ax[1].set_title("Vitesse de la comète en fonction du temps")
+    ax[1].set_xlabel("temps")
+    ax[1].set_ylabel("vitesse")
+    ax[1].legend()
     
     # Équation de l'énergie de la comète
     E = lambda t, q1, q2, q3, q4: (m/2) * (q3 ** 2 + q4 ** 2) - G * m ** 2 / np.sqrt((q1 ** 2 + q2 ** 2))
-    plt.plot(subdivision, [E(None, approx[0][t], approx[1][t], approx[2][t], approx[3][t]) for t in range(len(subdivision))])
+    ax[2].plot(subdivision, [E(None, approx_euler[0][t], approx_euler[1][t], approx_euler[2][t], approx_euler[3][t]) for t in range(len(subdivision))])
+    ax[2].set_title("Énergie de la comète en fonction du temps")
+    ax[2].set_xlabel("temps")
+    ax[2].set_ylabel("énergie")
+    ax[2].legend()
+    
+    filepath = './Results/trajectoires_cometeT={}'.format(T)
+    
+    plt.tight_layout()
+    plt.savefig(filepath)
+    print("Le graphique a été sauvegardé dans ", filepath)
     plt.show()
